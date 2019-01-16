36f273b08d73bd522feb610c5e2273d1a068a796
Marenda SA <renda_ardy@outlook.com>
2019-01-16 12:32:08 +0800

    Add image urls

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    { name: 'Salmon Creek', image: '' },
-    { name: 'Granite Hill', image: '' },
-    { name: 'Mountain Goat\'s Rest', image: '' }
-  ]
+  const campgrounds = [{
+    name: 'Salmon Creek',
+    image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'
+  },
+  {
+    name: 'Mountain Goat\'s Rest',
+    image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
+  }]
 
   res.render('campgrounds', {
     campgrounds: campgrounds
