1597f352c4268af891815f38550c3e531f3392e3
Marenda SA <renda_ardy@outlook.com>
2019-01-16 13:41:00 +0800

    Creating Campgrounds

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('landing')
-})
-
-app.get('/campgrounds', (req, res) => {
-  const campgrounds = [{
+const campgrounds = [
+  {
     name: 'Salmon Creek',
     image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
   },
@@ -19,11 +20,30 @@ app.get('/campgrounds', (req, res) => {
   {
     name: 'Mountain Goat\'s Rest',
     image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
-  }]
+  }
+]
 
+app.get('/', (req, res) => {
+  res.render('landing')
+})
+
+app.get('/campgrounds', (req, res) => {
   res.render('campgrounds', {
     campgrounds: campgrounds
   })
+})
+
+app.post('/campgrounds', (req, res) => {
+  let name = req.body.name
+  let image = req.body.image
+  let newCampground = { name: name, image: image }
+
+  campgrounds.push(newCampground)
+  res.redirect('/campgrounds')
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new')
 })
 
 app.listen('3000', () => {
