a24864c66a6b892a9ec25b2f471142dc70943794
Marenda SA <renda_ardy@outlook.com>
2019-01-16 14:07:21 +0800

    Styling Campgrounds

--- app.js
+++ app.js
@@ -20,6 +20,30 @@ const campgrounds = [
   {
     name: 'Mountain Goat\'s Rest',
     image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
+  },
+  {
+    name: 'Salmon Creek',
+    image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'
+  },
+  {
+    name: 'Mountain Goat\'s Rest',
+    image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
+  },
+  {
+    name: 'Salmon Creek',
+    image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
+  },
+  {
+    name: 'Granite Hill',
+    image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'
+  },
+  {
+    name: 'Mountain Goat\'s Rest',
+    image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
   }
 ]
 
