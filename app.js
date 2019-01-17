5c374ec82ad27bb294eda6f326e35329e83c4222
Marenda SA <renda_ardy@outlook.com>
2019-01-17 12:41:22 +0800

    Add data persistence

--- app.js
+++ app.js
@@ -1,59 +1,47 @@
 const express = require('express')
 const app = express()
-
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 app.set('view engine', 'ejs')
 
-const campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'
-  },
-  {
-    name: 'Mountain Goat\'s Rest',
-    image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
-  },
-  {
-    name: 'Salmon Creek',
-    image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'
-  },
-  {
-    name: 'Mountain Goat\'s Rest',
-    image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
-  },
-  {
-    name: 'Salmon Creek',
-    image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'
-  },
-  {
-    name: 'Granite Hill',
-    image: 'https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg'
-  },
-  {
-    name: 'Mountain Goat\'s Rest',
-    image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
-  }
-]
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true })
+
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+})
+const Campground = mongoose.model('Campground', campgroundSchema)
+
+// Campground.create(
+//   {
+//     name: 'Mountain Goat\'s Rest',
+//     image: 'https://farm3.staticflickr.com/2116/2164766085_0229ac3f08.jpg'
+//   }, (err, item) => {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log(item)
+//     }
+//   }
+// )
 
 app.get('/', (req, res) => {
   res.render('landing')
 })
 
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', {
-    campgrounds: campgrounds
+  Campground.find({}, (err, items) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('campgrounds', {
+        campgrounds: items
+      })
+    }
   })
 })
 
@@ -62,8 +50,13 @@ app.post('/campgrounds', (req, res) => {
   let image = req.body.image
   let newCampground = { name: name, image: image }
 
-  campgrounds.push(newCampground)
-  res.redirect('/campgrounds')
+  Campground.create(newCampground, (err, item) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect('/campgrounds')
+    }
+  })
 })
 
 app.get('/campgrounds/new', (req, res) => {
