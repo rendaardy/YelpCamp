2dfe463d2ec4343b0dc81965632fa1b5c4fb29cb
Marenda SA <renda_ardy@outlook.com>
2019-01-26 21:08:34 +0800

    Add comment edit and delete functionality

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,39 @@ router.post('/', isLoggedIn, (req, res) => {
   })
 })
 
+router.get('/:comment_id/edit', (req, res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if (err) {
+      res.redirect('back')
+    } else {
+      res.render('comments/edit', {
+        campground_id: req.params.id,
+        comment: foundComment
+      })
+    }
+  })
+})
+
+router.put('/:comment_id', (req, res) => {
+  Comment.updateOne({ _id: req.params.comment_id }, req.body.comment, (err, updated) => {
+    if (err) {
+      res.redirect('back')
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`)
+    }
+  })
+})
+
+router.delete('/:comment_id', (req, res) => {
+  Comment.deleteOne({ _id: req.params.comment_id }, (err) => {
+    if (err) {
+      res.redirect(`back`)
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`)
+    }
+  })
+})
+
 function isLoggedIn (req, res, next) {
   if (req.isAuthenticated()) {
     return next()
